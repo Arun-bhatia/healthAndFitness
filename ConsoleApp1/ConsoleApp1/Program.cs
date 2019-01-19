2ed4af74f4d7d77a4ed4e6f668747023bd62ab68
visualstudio <visualstudio@122-PC26>
2019-01-18 19:23:58 -0500

    new code added to calculate the BMI

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight, height;
+            weight = Convert.ToInt32(Console.ReadLine());
+            height = Convert.ToInt32(Console.ReadLine());
+            
+            int BMI = 0;
+            BMI = weight / (height * height);
+            Console.WriteLine("Your BMI is " + BMI);
+        }
+    }
+}
